3b4fce727af12d0f589b759240fb6692815522de
thinhcutoe <97089958+thinhcutoe@users.noreply.github.com>
2022-01-04 17:34:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,15 +6,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Làm Ny tớ nhaaaaaa 🥰',
+    desc: 'Thoát ra là đồng ý nhóeeeeeeeee ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Vì cậu đẹp trai vler <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Tối nay 7h, mình đi chơi nhaa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/thinh.superidol.2003' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
